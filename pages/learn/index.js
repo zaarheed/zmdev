5931da702d83b80cd511f254dc37988a431b50ad
Zahid <zahid@zmdev.com>
2022-11-12 21:50:16 +0000

    fixed svg error

--- pages/learn/index.js
+++ pages/learn/index.js
@@ -151,7 +151,7 @@ export default function Learn({ milestones = [] }) {
                         <DefaultButton
                             href={SUBSCRIPTION_LINK}
                             icon={(
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-hulk-500 duration-200 group-hover:scale-105" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-hulk-500 duration-200 group-hover:scale-105" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                     <path d="M10 17v2a2 2 0 0 1-2 2v0a2 2 0 0 1-2-2V5a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v3h3" />
                                     <path d="M22 17v2a2 2 0 0 1-2 2H8" />
                                     <path d="M19 17V5a2 2 0 0 0-2-2H4" />
