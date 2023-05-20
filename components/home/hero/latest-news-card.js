d482b5e65e01596e70a0dfe909ff19e31c60bdfd
Zahid Mahmood <zahid@zmdev.com>
2023-05-20 09:08:53 +0100

    Updated message in latest news card

--- components/home/hero/latest-news-card.js
+++ components/home/hero/latest-news-card.js
@@ -10,25 +10,24 @@ export default function LatestNewsCard() {
 					"group font-plex"
 				)}
 				target="_blank"
-				href="https://twitter.com/zaarheed/status/1446817675733241859"
+				href="https://twitter.com/zaarheed/status/1650867627584045058"
 				rel="noreferrer"
 			>
 				<p className="text-flesh-500 dark:text-zinc-500 font-medium text-xs">
                     Latest update
 				</p>
 				<p className="font-medium text-zinc-900 dark:text-zinc-100">
-                    I relaunched Buskana!
+                    Come work with me!
 				</p>
 				<p className="text-zinc-700 dark:text-zinc-200">
-                    Buskana is now a digital ordering platform
-                    for festivals &amp; events.{" "}
+                    I'm hiring engineers on a rolling basis. If you're interested in Healthcare and AI let's talk!
 					<span className="block text-azure-500 dark:text-mango-500 font-medium group-hover:underline">
                         Learn more
 					</span>
 				</p>
 			</a>
 			<p className="text-zinc-400 dark:text-zinc-300 text-sm mt-4">
-                Updated 24th September 2021
+                Updated 25th April 2023
 			</p>
 		</div>
 	)
