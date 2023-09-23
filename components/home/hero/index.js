15705ccb8f8fb683559b27fd5f3a8084f3bfb01c
Zahid <zahid@zmdev.com>
2023-09-23 18:04:32 +0100

    updated projects

--- components/home/hero/index.js
+++ components/home/hero/index.js
@@ -32,7 +32,7 @@ export default function Hero() {
                         not a real entrepreneur
 					</h2>
 					<p className="text-zinc-600 dark:text-zinc-300 font-plex">
-                        I build delightful digital experiences on the web to make the world a better place.
+                        I build software to solve big problems and make the world a better place.
 					</p>
 					<div className="w-full flex flex-row justify-start py-3">
 						<Technologies />
